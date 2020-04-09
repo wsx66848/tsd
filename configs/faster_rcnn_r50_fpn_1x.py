6430c27a6d4f708f82730b2883ef844937b6043e
wsx66848 <wsx66848@gmail.com>
2020-04-09 17:25:55 +0800

    [fix]修改名字为wsx

--- configs/faster_rcnn_r50_fpn_1x.py
+++ configs/faster_rcnn_r50_fpn_1x.py
@@ -173,7 +173,7 @@ total_epochs = 24
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 #work_dir = './work_dirs/faster_rcnn_r50_fpn_1x'
-work_dir = './work_dirs/backplane_batch_aug_rcnn_r50_fpn_1x_test'
+work_dir = './work_dirs/backplane_batch_aug_rcnn_r50_fpn_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
